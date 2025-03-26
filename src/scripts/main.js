843e9720c3cd36a63894da10a7a953cfe0446c8e
Rafael Gonçalves Ribeiro <raffafullstack@gmail.com>
2025-03-26 18:16:15 -0300

    definindo linguagem para português e criando algoritmo de switch case para traduzir os tipos obtidos da API

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -26,68 +26,146 @@ async function request(e){
         background.classList.add("type")
         switch(type1){
             case "bug":
+                type1 = 'Inseto'
                 background.classList.add("bug")
                 break;
             case "dark":
+                type1 = 'Sombrio'
                 background.classList.add("dark")
                 break;
             case "dragon":
+                type1 = 'Dragão'
                 background.classList.add("dragon")
                 break;
             case "electric":
+                type1 = 'Elétrico'
                 background.classList.add("electric")
                 break;
             case "fairy":
+                type1 = 'Fada'
                 background.classList.add("fairy")
                 break;
             case "fighting":
+                type1 = 'Lutador'
                 background.classList.add("fighting")
                 break;
             case "fire":
+                type1 = 'Fogo'
                 background.classList.add("fire")
                 break;
             case "flying":
+                type1 = 'Voador'
                 background.classList.add("flying")
                 break;
             case "ghost":
+                type1 = 'Fantasma'
                 background.classList.add("ghost")
                 break;
             case "grass":
+                type1 = 'Grama'
                 background.classList.add("grass")
                 break;
             case "ground":
+                type1 = 'Terra'
                 background.classList.add("ground")
                 break;
             case "ice":
+                type1 = 'Gelo'
                 background.classList.add("ice")
                 break;
             case "normal":
+                type1 = 'Normal'
                 background.classList.add("normal")
                 break;
             case "poison":
+                type1 = 'Veneno'
                 background.classList.add("poison")
                 break;
             case "psychic":
+                type1 = 'Psíquico'
                 background.classList.add("psychic")
                 break;
             case "rock":
+                type1 = 'Pedra'
                 background.classList.add("rock")
                 break;
             case "steel":
+                type1 = 'Ferro'
                 background.classList.add("steel")
                 break;
             case "water":
+                type1 = 'Água'
                 background.classList.add("water")
                 break;
         }
 
+        switch(type2){
+            case "bug":
+                type2 = 'Inseto'
+                break;
+            case "dark":
+                type2 = 'Sombrio'
+                break;
+            case "dragon":
+                type2 = 'Dragão'
+                break;
+            case "electric":
+                type2 = 'Elétrico'
+                break;
+            case "fairy":
+                type2 = 'Fada'
+                break;
+            case "fighting":
+                type2 = 'Lutador'
+                break;
+            case "fire":
+                type2 = 'Fogo'
+                break;
+            case "flying":
+                type2 = 'Voador'
+                break;
+            case "ghost":
+                type2 = 'Fantasma'
+                break;
+            case "grass":
+                type2 = 'Grama'
+                break;
+            case "ground":
+                type2 = 'Terra'
+                break;
+            case "ice":
+                type2 = 'Gelo'
+                break;
+            case "normal":
+                type2 = 'Normal'
+                break;
+            case "poison":
+                type2 = 'Veneno'
+                break;
+            case "psychic":
+                type2 = 'Psíquico'
+                break;
+            case "rock":
+                type2 = 'Pedra'
+                break;
+            case "steel":
+                type2 = 'Ferro'
+                break;
+            case "water":
+                type2 = 'Água'
+                break;
+            default:
+                type2 = null
+                break
+        }
+
         pokeInfo.innerHTML = `<img src="${sprite}" alt="${name}">`
         pokeInfo.innerHTML += `<img src="${spriteShiny}" alt="${name} shiny">`
-        pokeInfo.innerHTML += `<p>Name: ${name}</p>`
+        pokeInfo.innerHTML += `<p>Nome: ${name}</p>`
         pokeInfo.innerHTML += `<p>Pokedex ID: ${id}</p>`
-        if(type2){pokeInfo.innerHTML += `<p>Type: ${type1} / ${type2}</p>`}
-        else{pokeInfo.innerHTML += `<p>Type: ${type1}</p>`}
-        pokeInfo.innerHTML += `<p>Size: ${height/10}m || ${weight/10}kg</p>`
+        if(type2){pokeInfo.innerHTML += `<p>Tipo: ${type1} / ${type2}</p>`}
+        else{pokeInfo.innerHTML += `<p>Tipo: ${type1}</p>`}
+        pokeInfo.innerHTML += `<p>Tamanho: ${height/10}m || ${weight/10}kg</p>`
     }
     catch{
         pokeInfo.innerHTML = `<p>Pokemon inexistente</p>`
